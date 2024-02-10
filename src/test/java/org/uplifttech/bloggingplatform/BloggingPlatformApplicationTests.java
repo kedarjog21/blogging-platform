ae95e4b40945a1cc0d29ea2a5e875b5acea0503f
kedarjog21 <kedarjog.vjti@gmail.com>
2024-02-10 19:29:02 +0530

    initial commit

---
+++ src/test/java/org/uplifttech/bloggingplatform/BloggingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.uplifttech.bloggingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
